c048b97f66c46d05f18ae8881ca8f687072b2772
John <quandung2412@gmail.com>
2020-08-27 11:32:57 +0700

    update

--- src/pages/Admin/AdminPartner.js
+++ src/pages/Admin/AdminPartner.js
@@ -7,9 +7,28 @@ export default class AdminPartner extends Component {
         return (
             <div>
                 <NavBar/>
-                <div class="admin-partner">
-                    <h1>Các đối tác của công ty</h1>
-                    
+                <div class="admin-partner-content">
+                    <h1>Các công ty đối tác</h1>
+                    <table class='table-content'>
+                        <tr>
+                            <th>Tên Công Ty</th>
+                            <th>Địa Chỉ</th>
+                            <th>Hạn Mức</th>
+                            <th></th>
+                        </tr>
+                        <tr>
+                            <td>Công ty THHH A</td>
+                            <td>Số 3 Tôn Thất Thuyết</td>
+                            <td>200.000.000 VND</td>
+                            <td><button type='button'>Xem chi tiết</button></td>
+                        </tr>
+                        <tr>
+                            <td>Công ty THHH B</td>
+                            <td>Số 3 Tôn Thất Thuyết</td>
+                            <td>1.000.000.000 VND</td>
+                            <td><button type='button'>Xem chi tiết</button></td>
+                        </tr>
+                    </table>
                 </div>
             </div>
         )
