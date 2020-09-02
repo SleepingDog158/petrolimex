a508588374f044cf2d556b65842d2e3210590379
John <quandung2412@gmail.com>
2020-09-02 21:48:58 +0700

    Update

--- src/pages/Admin/AdminPartner.js
+++ src/pages/Admin/AdminPartner.js
@@ -9,35 +9,6 @@ export default class AdminPartner extends Component {
         return (
             <div>
                 <NavBar/>
-                {/*
-                <div className="admin-partner-content">
-                    <h1>Các công ty đối tác</h1>
-                    <table className='admin-table-content'>
-                        <tr>
-                            <th className="admin-table-header">Tên Công Ty</th>
-                            <th className="admin-table-header">Địa Chỉ</th>
-                            <th className="admin-table-header">Hạn Mức</th>
-                            <th className="admin-table-header"></th>
-                        </tr>
-                        <tr>
-                            <td className="admin-table-row">Công ty THHH A</td>
-                            <td className="admin-table-row">Số 3 Tôn Thất Thuyết</td>
-                            <td className="admin-table-row">200.000.000 VND</td>
-                            <td className="admin-table-row">
-                                <button type='button'>Xem chi tiết</button>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td className="admin-table-row">Công ty THHH B</td>
-                            <td className="admin-table-row">Số 3 Tôn Thất Thuyết</td>
-                            <td className="admin-table-row">1.000.000.000 VND</td>
-                            <td className="admin-table-row">
-                                <button type='button'>Xem chi tiết</button>
-                            </td>
-                        </tr>
-                    </table>
-                </div>
-                */}
                 <div className="admin-partner-content">
                     <h1 className="admin-partner-header">Các công ty đối tác</h1>
                     <button type="button" class="admin-partner-button"><FontAwesomeIcon icon={faPlusCircle} className="admin-partner-icon"/></button>
