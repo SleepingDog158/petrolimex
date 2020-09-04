ff44a949a406fc343b6779f0c07e29d85bb04ada
John <quandung2412@gmail.com>
2020-09-04 10:50:16 +0700

    update

---
+++ src/components/StationList.js
@@ -0,0 +1,17 @@
+import React, { useEffect, useMemo, useState, Component } from 'react'
+import axios from "axios"
+import ModalEdit from "./ModalExample"
+
+export const StationList = () => {
+    useEffect(async() => {
+        const result = await axios.get("https://4x5nd.sse.codesandbox.io/stations");
+        console.log(result.data);
+        setStations(result.data);
+    }, []);
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
--- src/pages/Admin/AdminStation.js
+++ src/pages/Admin/AdminStation.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import NavBar from '../../components/NavBar'
+import { StationList } from '../../components/StationList'
 
 export default class AdminStation extends Component {
     render() {
@@ -8,26 +9,7 @@ export default class AdminStation extends Component {
                 <NavBar/>
                 <div className='admin-station-content'>
                     <h1>Các chi nhánh của công ty</h1>
-                    <table>
-                        <tr>
-                            <th>Tên Chi Nhánh</th>
-                            <th>Địa chỉ</th>
-                            <th>Thời gian làm việc</th>
-                            <th></th>
-                        </tr>
-                        <tr>
-                            <td>Cửa hàng xăng dầu A</td>
-                            <td>Số 3 Tôn Thất Thuyết</td>
-                            <td>06:00-22:00</td>
-                            <td><button type='button'>Xem chi tiết</button></td>
-                        </tr>
-                        <tr>
-                            <td>Cửa hàng xăng dầu B</td>
-                            <td>Số 200 Cầu Giấy</td>
-                            <td>05:00-21:00</td>
-                            <td><button type='button'>Xem chi tiết</button></td>
-                        </tr>
-                    </table>
+                <div><StationList/></div>
                 </div>
             </div>
         )
