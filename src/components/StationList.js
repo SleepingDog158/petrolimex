2d07bc7f95b72999b1302d28d128128eb051efef
John <quandung2412@gmail.com>
2020-09-09 10:32:41 +0700

    update

--- src/components/StationList.js
+++ src/components/StationList.js
@@ -1,33 +1,38 @@
 import React, { useEffect, useMemo, useState, Component } from 'react'
 import axios from "axios"
 import ModalEdit from "./ModalExample"
-import { ToastContainer, toast } from "react-toastify"
 import Table from "react-bootstrap/Table"
 import { TableHeader } from "./TableHeader"
+import { Search } from "./Search"
+import { PaginationComponent } from "./PaginationComponent"
 
-toast.configure()
 export const StationList = () => {
 
-    const [station, setStations] = useState([]);
+    const [station, setStation] = useState([]);
     const [modal, setModal] = useState([false]);
     const [id, setId] = useState([]);
+    const [currentStation, setCurrentStation] = useState(null);
     const [station_name, setStationName] = useState([]);
     const [station_address, setStationAddress] = useState([]);
     const [station_working_time, setStationWorkingTime] = useState([]);
     const [sorting, setSorting] = useState({ field: "", order: ""});
+    const [search, setSearch] = useState("");
+    const ITEM_PER_PAGE = 10;
+    const [currentPage, setCurrentPage] = useState(1);
+    const [totalItem, setTotalItem] = useState(0)
 
     const headers = [
         { name: "ID", field: "id", sortable: true },
         { name: "Tên cửa hàng", field: "station_name", sortable: true },
         { name: "Địa chỉ", field: "station_address", sortable: false },
         { name: "Thời gian làm việc", field: "station_working_time", sortable: false },
-        { name: "" },
+        { name: "", sortable: false },
     ];
 
     const toggle = (setStation) => {
         setModal(!modal);
         if (!modal) {
-            setStation(station);
+            setCurrentStation(station);
             setId(station.id);
             setStationName(station.station_name);
             setStationAddress(station.station_address);
@@ -36,26 +41,41 @@ export const StationList = () => {
     }
     
     useEffect(async() => {
-        const result = await axios.get("https://tnzio.sse.codesandbox.io/stations");
+        const result = await axios.get("https://tnzio.sse.codesandbox.io/station");
         console.log(result.data);
-        setStations(result.data);
+        setStation(result.data);
     }, []);
 
     const stationData = useMemo(() => {
         let processedStation = station;
+        if (search) {
+            processedStation = processedStation.filter((station) =>
+                station.id.includes(search) || station.station_name.toLowerCase().includes(search.toLowerCase())
+            );
+        }
+        setTotalItem(processedStation.length);
         if (sorting.field) {
             const reversed = sorting.order === "asc" ? 1 : -1;
             processedStation = processedStation.sort(
                 (a, b) => reversed * a[sorting.field].localeCompare(b[sorting.field])
             );
         }
-        return processedStation.slice();
-    }, [station, sorting]);
+        return processedStation.slice(
+            (currentPage - 1) * ITEM_PER_PAGE,
+            (currentPage - 1) * ITEM_PER_PAGE + ITEM_PER_PAGE
+        );
+    }, [station, currentPage, search, sorting]);
 
     return (
         <div>
             <div>
                 <div>
+                    <div>
+                        <Search onSearch={(value) => {
+                            setSearch(value);
+                            setCurrentPage(1);
+                        }} />
+                    </div>
                     <Table striped>
                         <TableHeader
                             headers={headers}
@@ -80,6 +100,13 @@ export const StationList = () => {
                             ))}
                         </tbody>
                     </Table>
+                    <div>
+                        <PaginationComponent
+                            total={totalItem}
+                            itemPerPage={ITEM_PER_PAGE}
+                            currentPage={currentPage}
+                            onPageChange={(page) => setCurrentPage(page)} />
+                    </div>
                 </div>
             </div>
         </div>
--- src/pages/Admin/AdminPartner.js
+++ src/pages/Admin/AdminPartner.js
@@ -3,6 +3,7 @@ import NavBar from "../../components/NavBar"
 import "../../components/Admin.css"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faPlusCircle, faSearch } from "@fortawesome/free-solid-svg-icons"
+import { PartnerList } from '../../components/PartnerList'
 
 export default class AdminPartner extends Component {
     render() {
@@ -12,12 +13,14 @@ export default class AdminPartner extends Component {
                 <div className="admin-partner-content">
                     <div>
                         <h1 className="admin-partner-header">Các công ty đối tác</h1>
-                        <input type="text" className="admin-partner-search-input" placeholder="Tên công ty"/>
+                        {/*<input type="text" className="admin-partner-search-input" placeholder="Tên công ty"/>
                         <button type="button" className="admin-partner-search-button"><FontAwesomeIcon icon={faSearch}/></button>
                     </div>
                     <button type="button" className="admin-partner-button"><FontAwesomeIcon icon={faPlusCircle} className="admin-partner-icon"/></button>
                     <button type="button" className="admin-partner-button">Công ty TNHH A</button>
-                    <button type="button" className="admin-partner-button">Công ty TNHH B</button>
+                    <button type="button" className="admin-partner-button">Công ty TNHH B</button>*/}
+                        <div><PartnerList/></div>
+                    </div>
                 </div>
             </div>
         )
